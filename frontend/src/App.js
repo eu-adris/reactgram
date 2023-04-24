b4efe93f8941a431a7110181a9d3be1dde9d11bc
adriano <noturnoabs@hotmail.com>
2023-04-24 16:54:30 -0300

    global css add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 //Router
-import {BrowserRouter, Routes, Route, Navigate} from  'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/Register' element={<Register />}/>
-      </Routes>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/Register' element={<Register />} />
+          </Routes>
+        </div>
+        < Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,8 +1,32 @@
 import "./Auth.css"
 
+//components
+import {Link} from 'react-router-dom'
+
+//Hooks
+import { useState, useEffect } from "react"
+
 const Register = () => {
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos</p>
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Nome" />
+      <input type="email" placeholder="E-mail" />
+      <input type="password" placeholder="senha" />
+      <input type="password" placeholder="Confirme a senha" />
+      <input type="submit" value='cadastrar'/>
+      </form>
+      <p>
+        ja tem conta? <Link to='/login'>Clique aqui</Link>
+      </p>
+    </div>
   )
 }
 
